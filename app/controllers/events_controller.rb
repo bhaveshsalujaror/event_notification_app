f71ee918738d8830a4b9380fc5e6d44e54da284b
Bhavesh <bhavesh.saluja@bacancy.com>
2024-02-09 11:00:46 +0530

    Added Event Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def event_a
+    # Implement logic to create Event A
+    # This could involve making a request to Iterable's API
+    # For now, just render a message
+
+    render plain: "Event A created"
+  end
+
+  def event_b
+    render plain: "Event B created"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root to: "events#index"
 
   devise_for :users
+
+  get 'events/event_a', to: 'events#event_a', as: 'create_event_a'
+  get 'events/event_b', to: 'events#event_b', as: 'create_event_b'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
