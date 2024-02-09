879390fc6c7b7ce4ee90a90162ef5f010b8f00fc
Bhavesh <bhavesh.saluja@bacancy.com>
2024-02-09 12:25:02 +0530

    added Event create form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,39 @@
 class EventsController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
+    @event_a = Event.new
+    @event_b = Event.new
   end
 
   def event_a
-    # Implement logic to create Event A
-    # This could involve making a request to Iterable's API
-    # For now, just render a message
+    @user = current_user
+    @event = @user.events.build(event_params)
 
-    render plain: "Event A created"
+    if @event.save
+      flash[:notice] = "Event A Created Successfully"
+      redirect_to root_path
+    else
+      render :new # Render the new form again if validation fails
+    end
   end
 
   def event_b
-    render plain: "Event B created"
+    @user = current_user
+    @event = @user.events.build(event_params)
+
+    if @event.save
+      flash[:notice] = "Event B Created Successfully"
+      redirect_to root_path
+    else
+      render :new # Render the new form again if validation fails
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get 'events/event_a', to: 'events#event_a', as: 'create_event_a'
   get 'events/event_b', to: 'events#event_b', as: 'create_event_b'
 
+  post 'events/event_a', to: 'events#event_a'
+  post 'events/event_b', to: 'events#event_b'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
