68f6a9aaac03c78e8850e1db4907377c8e0f09f8
Bhavesh <bhavesh.saluja@bacancy.com>
2024-02-09 14:08:45 +0530

    code refactored

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,16 +3,20 @@ require 'webmock'
 class EventsController < ApplicationController
   include WebMock::API
   WebMock.enable!
-  
+
   before_action :authenticate_user!
 
+  ITERABLE_EVENTS_TRACK_URL = "https://api.iterable.com/api/events/track".freeze
+  ITERABLE_EMAIL_TARGET_URL = "https://api.iterable.com/api/email/target".freeze
+
+  # Action to display all events
   def index
     @events = Event.all
-
     @event_a = Event.new
     @event_b = Event.new
   end
 
+  # Action to create Event A
   def event_a
     @user = current_user
     @event = @user.events.build(event_params)
@@ -21,12 +25,13 @@ class EventsController < ApplicationController
       flash[:notice] = "Event A Created Successfully"
       send_event_to_iterable(@event.title, @user)
     else
-      flash[:alert] = "Event B Creation Unsuccessfull"
+      flash[:alert] = "Event A Creation Unsuccessful"
     end
     
     redirect_to root_path
   end
 
+  # Action to create Event B
   def event_b
     @user = current_user
     @event = @user.events.build(event_params)
@@ -34,76 +39,55 @@ class EventsController < ApplicationController
     if @event.save
       flash[:notice] = "Event B Created Successfully"
       send_event_to_iterable(@event.title, @user)
-
-      # Send email notification for Event B
       send_email_notification(@user, @event)
-
       flash[:alert] = "Email For Event B Sent Successfully to #{@user.email}"
-
     else
-      flash[:alert] = "Event B Creation Unsuccessfull"
+      flash[:alert] = "Event B Creation Unsuccessful"
     end
     
     redirect_to root_path
   end
 
-  def send_notifications_for_event_b
-    # Logic to send email notifications for Event B using the Iterable API
-    # This could include making requests to the Iterable API
-    # Not implemented as per your requirement
-    flash[:notice] = "Email notifications sent for Event B!"
-    redirect_to root_path
-  end
-
   private
 
+  # Strong parameters for event creation
   def event_params
     params.require(:event).permit(:title, :description)
   end
 
+  # Method to send event data to Iterable
   def send_event_to_iterable(event_name, user)
-    # Mocking the API request to iterable.com for testing purposes
-    event_and_user_detail = { event_name: event_name, user_email: user.email}
+    event_and_user_detail = { event_name: event_name, user_email: user.email }
 
-    stub_request(:post, "https://api.iterable.com/api/events/track").
-      with(
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Ruby'
-        }).
+    # Stubbing the request for testing purposes
+    stub_request(:post, ITERABLE_EVENTS_TRACK_URL).
+      with(headers: { 'Accept'=>'*/*', 'User-Agent'=>'Ruby' }).
       to_return(status: 200, body: event_and_user_detail.to_json, headers: {})
 
-    HTTParty.post("https://api.iterable.com/api/events/track")
+    # Sending the event data to Iterable
+    HTTParty.post(ITERABLE_EVENTS_TRACK_URL)
   end
 
+  # Method to send email notification using Iterable
   def send_email_notification(user, event)
-    # Make a request to the Iterable API to send an email notification
     email_data = {
       campaignId: event.id,
       recipientEmail: user.email,
       recipientUserId: user.id, # Assuming user.id is the user's Iterable user ID
-      dataFields: {
-        event_name: event.title,
-        event_description: event.description
-      },
+      dataFields: { event_name: event.title, event_description: event.description },
       sendAt: DateTime.now, # Optional send time
       allowRepeatMarketingSends: true,
       metadata: {}
     }
 
-    stub_request(:post, "https://api.iterable.com/api/email/target").
-      with(
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Ruby'
-        }).
-      to_return(status: 200, body: {"msg": "Email Sent", "code": "Success", "params": email_data }.to_json  , headers: {})
+    # Stubbing the request for testing purposes
+    stub_request(:post, ITERABLE_EMAIL_TARGET_URL).
+      with(headers: { 'Accept'=>'*/*', 'User-Agent'=>'Ruby' }).
+      to_return(status: 200, body: { msg: "Email Sent", code: "Success", params: email_data }.to_json, headers: {})
 
-    HTTParty.post("https://api.iterable.com/api/email/target", body: email_data.to_json, headers: { 'Content-Type' => 'application/json' })
+    # Sending the email data to Iterable
+    HTTParty.post(ITERABLE_EMAIL_TARGET_URL, body: email_data.to_json, headers: { 'Content-Type' => 'application/json' })
 
     # Handle the response if needed
   end
-
 end
